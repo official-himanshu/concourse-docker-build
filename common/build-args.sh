ce5bf1185f75b4ff88a28ba43db205cfca0e3b06
himanshuchaudhary426 <himanshu.chaudhary@knoldus.com>
2021-09-10 17:50:00 +0530

    changes check resource time

---
+++ common/build-args.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+printf '{"USERNAME": "%s", "PASSWORD": "%s", "URL": "%s"}\n'  \
+  "${ARTIFACTORY_USERNAME}}" \
+  "${ARTIFACTORY_PASSWORD}" \
+  "${ARTIFACTORY_URL}" \
+  > build-args.
